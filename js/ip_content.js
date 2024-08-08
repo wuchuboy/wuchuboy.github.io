61020e0f7b633a4759802719aa8132895df83431
ryancwu <ryancwu@tencent.com>
2024-08-09 03:03:43 +0800

    Site updated: 2024-08-09 03:03:43

--- js/ip_content.js
+++ js/ip_content.js
@@ -1,5 +1,3 @@
-
-
 //获取当前IP地址和浏览器标识
 function getbrowserInfo() {
     var agent = navigator.userAgent.toLowerCase();
@@ -30,23 +28,17 @@ function getbrowserInfo() {
     }
 }
 
-var ip_content = document.querySelector(".ip_content");
 
+function IPCallBack(data) {
+    var ip_content = document.querySelector(".ip_content");
+    ip_content.innerHTML = '欢迎来自 <span class="p" style=" color:red ">' + data["addr"] + "</span> 的小伙伴<br>" + '访问IP为： <span class="p" style=" color:blue " >' + data["ip"] + '</span><br>浏览器版本：<span class="p" style=" color:green ">' + getbrowserInfo() + '</span>';
+}
 
+function loadIPData() {
+    var script = document.createElement('script');
+    script.src = 'https://whois.pconline.com.cn/ipJson.jsp'; // 替换为实际的 JSONP URL
+    document.body.appendChild(script);
+}
 
-fetch('https://whois.pconline.com.cn/ipJson.jsp?json=true')
-  .then(response => response.json())
-  .then(data => {
-    // data就是我们请求的repos
-    console.log(data)
-  });
-
-
-
-
-
-// if (ip_content != null && typeof (response) != undefined) {
-//     ip_content.innerHTML = '欢迎来自 <span class="p" style=" color:red ">' + response["addr"] + "</span> 的小伙伴<br>" + '访问IP为： <span class="p" style=" color:blue " >' + response["ip"] + '</span><br>浏览器版本：<span class="p" style=" color:green ">' + getbrowserInfo() + '</span>';
-// }
-
-
+// 调用函数加载 IP 数据
+loadIPData();
