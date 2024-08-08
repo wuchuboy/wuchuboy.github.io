7e853b20ab83adba2e23ea55d43a14125e11e981
ryancwu <ryancwu@tencent.com>
2024-08-08 22:51:42 +0800

    Site updated: 2024-08-08 22:51:42

--- js/myfooter.js
+++ js/myfooter.js
@@ -24,7 +24,7 @@ $('.framework-info').html('本站已运行<span id="span_dt_dt" style="color: #f
   e_minsold=(e_hrsold-hrsold)*60;
   minsold=Math.floor((e_hrsold-hrsold)*60);
   seconds=Math.floor((e_minsold-minsold)*60);
-  span_dt_dt.innerHTML='<font style=color:#afb4db>'+daysold+'</font> 天 <font style=color:#f391a9>'+hrsold+'</font> 时 <font style=color:#fdb933>'+minsold+'</font> 分 <font style=color:#a3cf62>'+seconds+'</font> 秒';
+  span_dt_dt.innerHTML='<font style=color:#0101DF>'+daysold+'</font> 天 <font style=color:#f391a9>'+hrsold+'</font> 时 <font style=color:#fdb933>'+minsold+'</font> 分 <font style=color:#a3cf62>'+seconds+'</font> 秒';
 }
 
 
